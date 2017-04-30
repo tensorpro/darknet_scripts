fb99ceabf904940118151da23b6c0e178f5c096a
Akshay Mishra <lck.akshay@gmail.com>
2017-04-29 19:24:38 -0500

    updated script

--- scratch.py
+++ scratch.py
@@ -1,10 +1,15 @@
 import numpy as np
 from darkflow.net.build import TFNet
-from _darkflow.net.build import TFNet
 
 options = {"model": "cfg/yolo.cfg", "load": "yolo.weights", "threshold": 0.1}
-
+net = TFNet(options)
 with net.graph.as_default() as g:
-    l = net.states[-5].out
+    l = net.outputs[-5].out
     loss  = l
-    iloss = net.intermediate(np.zeros((400,400,3)), l, loss)
+    iloss = net.inter_grad(np.zeros((400,400,3)), l, loss)
+    print("Identity loss")
+    print(iloss)
+    loss  = net.out
+    iloss = net.inter_grad(np.zeros((400,400,3)), l, loss)
+    print("loss with respect to output")
+    print(iloss)
